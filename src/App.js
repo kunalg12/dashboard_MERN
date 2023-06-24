79da2d2b953e7ee43db4d2b957bfa71d6c226146
kunalg12 <kunalgursal2923@gmail.com>
2023-06-24 22:32:48 +0530

    layout and scenes

--- src/App.js
+++ src/App.js
@@ -2,26 +2,27 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "theme";
 import { useSelector } from "react-redux";
-import { useMemo } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import { Routes } from "react-router-dom";
-import { Route } from "react-router-dom";
+import { useMemo } from "react";
+import Dashboard from "scenes/dashboard";
+import Layout from "scenes/layout";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 
 function App() {
-
   const mode = useSelector((state) => state.global.mode);
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="app">
       <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <CssBaseline/>
-        <Routes>
-          <Route element={<Layout/>}>
-            
-          </Route>
-        </Routes>
-      </ThemeProvider>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />}>
+                <Route path="/dashboard" element={<Dashboard />} />
+              </Route>
+            </Route>
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
---
+++ src/components/FlexBetween.jsx
@@ -0,0 +1,3 @@
+
+
+const FlexBetween =  styled
